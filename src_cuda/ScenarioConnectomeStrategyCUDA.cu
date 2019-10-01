ba54ebb66d1440c3772b6b918fd6a524defe7bb9
Jeffrey Kelling <j.kelling@hzdr.de>
2019-10-01 18:13:35 +0200

    ScenarioConnectomeStrategyCUDA: Fix zeroKernel call n

--- src_cuda/ScenarioConnectomeStrategyCUDA.cu
+++ src_cuda/ScenarioConnectomeStrategyCUDA.cu
@@ -108,7 +108,10 @@ void ScenarioConnectomeStrategyCUDA::derivatives() {
 
 	static constexpr int BLOCK_DIM = 128;
   // set derivatives of all voxels to zero
-	zeroKernel<<<m_this->nall/BLOCK_DIM + (m_this->nall%BLOCK_DIM>0), BLOCK_DIM>>>(deriv, m_this->nall);
+	{
+		const size_t n = ScenarioConnectomeAgents::num_agents*m_this->nall;
+		zeroKernel<<<n/BLOCK_DIM + (n%BLOCK_DIM>0), BLOCK_DIM>>>(deriv, n);
+	}
 
 	const dim3 blocks(m_this->nvl[0]/BLOCK_DIM + (m_this->nvl[0]%BLOCK_DIM>0), m_this->nvl[1], m_this->nvl[2]);
 	derivativeKernel<<<blocks, BLOCK_DIM>>>(agent, deriv, type, arr_prop, m_this->nall,m_this->dt, m_this->step);
