583757399550dc8637274876506b97e55ca84c3f
Jeffrey Kelling <j.kelling@hzdr.de>
2019-10-02 12:18:57 +0200

    ScenarioConnectomeStrategyCUDA: Add mallocPitch

--- src_cuda/ScenarioConnectomeStrategyCUDA.cu
+++ src_cuda/ScenarioConnectomeStrategyCUDA.cu
@@ -16,6 +16,7 @@ struct Coord {
 
 __constant__ ScenarioConnectome::properties prop;
 __constant__ int nvl[ndim];
+__constant__ ScenarioConnectomeStrategyCUDA::AllocPitch pitch;
 
 static __device__ constexpr int tissue(int type)
 {
@@ -27,27 +28,48 @@ double dt, int step);
 
 static __global__ void updateKernel(double* agent, const double* deriv, const int* type, const ScenarioConnectomeStrategyCUDA::array_properties arr_prop, double dt, int nall);
 
+size_t ScenarioConnectomeStrategyCUDA::nvx()
+{
+	return m_this->nvl[0]+2;
+}
+size_t ScenarioConnectomeStrategyCUDA::nvyz()
+{
+	return (m_this->nvl[1]+2)*(m_this->nvl[2]+2);
+}
+
 ScenarioConnectomeStrategyCUDA::ScenarioConnectomeStrategyCUDA(ScenarioConnectome* pthis)
 	: ScenarioConnectomeAbstractStrategy(pthis)
 {
+	const size_t width = this->nvx();
+	const size_t height = this->nvyz();
 	CUDA_SAFE_CALL(
-		cudaMalloc(&arr_prop.Dtau, ndim*sizeof(double)*m_this->nall)
+		cudaMallocPitch(&arr_prop.Dtau, &m_allocPitch.pDouble, width*sizeof(double), height*ndim)
 		);
+	std::cout << "pitch Dtau: " << m_allocPitch.pDouble << std::endl;
 	CUDA_SAFE_CALL(
-		cudaMalloc(&agent, ScenarioConnectomeAgents::num_agents*sizeof(double)*m_this->nall)
+		cudaMallocPitch(&agent, &m_allocPitch.pDouble, width*sizeof(double), height*ScenarioConnectomeAgents::num_agents)
 		);
+	std::cout << "pitch agent: " << m_allocPitch.pDouble << std::endl;
 	CUDA_SAFE_CALL(
-		cudaMalloc(&deriv, ScenarioConnectomeAgents::num_agents*sizeof(double)*m_this->nall)
+		cudaMallocPitch(&deriv, &m_allocPitch.pDouble, width*sizeof(double), height*ScenarioConnectomeAgents::num_agents)
 		);
+	std::cout << "pitch deriv: " << m_allocPitch.pDouble << std::endl;
 	CUDA_SAFE_CALL(
-		cudaMalloc(&type, sizeof(int)*m_this->nall)
+		cudaMallocPitch(&type, &m_allocPitch.pInt, width*sizeof(int), height)
 		);
+	std::cout << "pitch type: " << m_allocPitch.pInt << std::endl;
+
+	m_allocPitch.pDouble /= sizeof(double);
+	m_allocPitch.pInt /= sizeof(int);
 
 	CUDA_SAFE_CALL(
 		cudaMemcpyToSymbol(prop, &m_this->prop, sizeof(ScenarioConnectome::properties))
 		);
 	CUDA_SAFE_CALL(
 		cudaMemcpyToSymbol(nvl, (void*)m_this->nvl.data(), sizeof(int)*m_this->nvl.size())
+		);
+	CUDA_SAFE_CALL(
+		cudaMemcpyToSymbol(pitch, &m_allocPitch, sizeof(AllocPitch))
 		);
 }
 
@@ -69,32 +91,53 @@ ScenarioConnectomeStrategyCUDA::~ScenarioConnectomeStrategyCUDA()
 
 void ScenarioConnectomeStrategyCUDA::push()
 {
+	const size_t width = this->nvx();
+	const size_t height = this->nvyz();
 	for(int a = 0; a < ndim; ++a)
 	{
 		CUDA_SAFE_CALL(
-			cudaMemcpy(arr_prop.Dtau+m_this->nall*a, m_this->arr_prop.Dtau[a].data(), m_this->nall*sizeof(double), cudaMemcpyHostToDevice)
+			/*cudaMemcpy(arr_prop.Dtau+m_this->nall*a, m_this->arr_prop.Dtau[a].data(), m_this->nall*sizeof(double), cudaMemcpyHostToDevice)*/
+			cudaMemcpy2D(arr_prop.Dtau+height*m_allocPitch.pDouble*a, m_allocPitch.pDouble*sizeof(double)
+				, m_this->arr_prop.Dtau[a].data(), width*sizeof(double), width*sizeof(double), height
+				, cudaMemcpyHostToDevice)
 			);
 	}
 	for(int a = 0; a < ScenarioConnectomeAgents::num_agents; ++a)
 	{
 		CUDA_SAFE_CALL(
-			cudaMemcpy(agent+m_this->nall*a, m_this->agent[a].data(), m_this->nall*sizeof(double), cudaMemcpyHostToDevice)
+			/*cudaMemcpy(agent+m_this->nall*a, m_this->agent[a].data(), m_this->nall*sizeof(double), cudaMemcpyHostToDevice)*/
+			cudaMemcpy2D(agent+height*m_allocPitch.pDouble*a, m_allocPitch.pDouble*sizeof(double)
+				, m_this->agent[a].data(), width*sizeof(double), width*sizeof(double), height
+				, cudaMemcpyHostToDevice)
 			);
 	}
 	CUDA_SAFE_CALL(
-		cudaMemcpy(type, m_this->type.data(), m_this->nall*sizeof(int), cudaMemcpyHostToDevice)
+		/*cudaMemcpy(type, m_this->type.data(), m_this->nall*sizeof(int), cudaMemcpyHostToDevice)*/
+		cudaMemcpy2D(type, m_allocPitch.pInt*sizeof(int)
+			, m_this->type.data(), width*sizeof(int), width*sizeof(int), height
+			, cudaMemcpyHostToDevice)
 		);
 }
 
 void ScenarioConnectomeStrategyCUDA::pop()
 {
+	const size_t width = this->nvx();
+	const size_t height = this->nvyz();
 	for(int a = 0; a < ScenarioConnectomeAgents::num_agents; ++a)
 	{
 		CUDA_SAFE_CALL(
-			cudaMemcpy(m_this->agent[a].data(), agent + m_this->nall*a, m_this->nall*sizeof(double), cudaMemcpyDeviceToHost)
+			/*cudaMemcpy(m_this->agent[a].data(), agent + m_this->nall*a, m_this->nall*sizeof(double), cudaMemcpyDeviceToHost)*/
+			cudaMemcpy2D(m_this->agent[a].data(), width*sizeof(double)
+				, agent+height*m_allocPitch.pDouble*a, m_allocPitch.pDouble*sizeof(double)
+				, width*sizeof(double), height
+				, cudaMemcpyDeviceToHost)
 			);
 		CUDA_SAFE_CALL(
-			cudaMemcpy(m_this->deriv[a].data(), deriv+m_this->nall*a, m_this->nall*sizeof(double), cudaMemcpyDeviceToHost)
+			/*cudaMemcpy(m_this->deriv[a].data(), deriv+m_this->nall*a, m_this->nall*sizeof(double), cudaMemcpyDeviceToHost)*/
+			cudaMemcpy2D(m_this->deriv[a].data(), width*sizeof(double)
+				, deriv+height*m_allocPitch.pDouble*a, m_allocPitch.pDouble*sizeof(double)
+				, width*sizeof(double), height
+				, cudaMemcpyDeviceToHost)
 			);
 	}
 }
@@ -121,14 +164,14 @@ void ScenarioConnectomeStrategyCUDA::derivatives() {
 	}
 #endif
 
-	//const dim3 blocks(m_this->nvl[0]/BLOCK_DIM + (m_this->nvl[0]%BLOCK_DIM>0), m_this->nvl[1], m_this->nvl[2]);
-	//derivativeKernel<<<blocks, BLOCK_DIM>>>(agent, deriv, type, arr_prop, m_this->nall,m_this->dt, m_this->step);
-  derivativeKernel<<<m_this->nall/BLOCK_DIM + (m_this->nall%BLOCK_DIM>0), BLOCK_DIM>>>(agent, deriv, type, arr_prop, m_this->nall,m_this->dt, m_this->step);
+	const dim3 blocks(m_this->nvl[0]/BLOCK_DIM + (m_this->nvl[0]%BLOCK_DIM>0), m_this->nvl[1], m_this->nvl[2]);
+	derivativeKernel<<<blocks, BLOCK_DIM>>>(agent, deriv, type, arr_prop, m_this->nall,m_this->dt, m_this->step);
+  //derivativeKernel<<<m_this->nall/BLOCK_DIM + (m_this->nall%BLOCK_DIM>0), BLOCK_DIM>>>(agent, deriv, type, arr_prop, m_this->nall,m_this->dt, m_this->step);
 }
 
-static __device__ int find_id(int i, int j, int k)
+static __device__ int find_id(int i, int j, int k, int p = pitch.pDouble)
 {
-	return i + (nvl[0] + 2) * (j + (nvl[1] + 2) * k);
+	return i + (p) * (j + (nvl[1] + 2) * k);
 }
 
 static __device__ Coord find_coord(int i)
@@ -146,10 +189,13 @@ static __device__ Coord find_coord(int i)
 static __global__ void derivativeKernel(const double* agent, double* deriv, const int* type, const ScenarioConnectomeStrategyCUDA::array_properties arr_prop, int nall,
 double dt, int step)
 {
-  const int i = threadIdx.x + blockDim.x*blockIdx.x;
-  Coord coord = find_coord(i);
- 
-  if(i < nall) {
+  const int ii = threadIdx.x + blockDim.x*blockIdx.x +1;
+  const int jj = blockIdx.y +1;
+  const int kk = blockIdx.z +1;
+  nall = pitch.pDouble * (nvl[1]+2)*(nvl[2]+2);
+  if(ii < nvl[0]+1)
+  {
+    const int i = find_id(ii,jj,kk);
 
     if (type[i] & tissue(EMP)) return;
 
@@ -259,10 +305,15 @@ void ScenarioConnectomeStrategyCUDA::update() {
 
 static __global__ void updateKernel(double* agent, const double* deriv, const int* type, const ScenarioConnectomeStrategyCUDA::array_properties arr_prop, double dt, int nall)
 {
-	const int i = threadIdx.x + blockDim.x*blockIdx.x;
+	int i = threadIdx.x + blockDim.x*blockIdx.x;
 	//const int jj = blockIdx.y +1;
 	//const int kk = blockIdx.z +1;
   if(i < nall) {
+	{
+		const int height = nall/(nvl[0]+2);
+		i = i/height*pitch.pDouble + i%height;
+		nall = pitch.pDouble * height;
+	}
     
     if (type[i] & tissue(EMP)) return;
     
