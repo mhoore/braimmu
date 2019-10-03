f18339b9167218aedc1da102039e1c621c76958e
Jeffrey Kelling <j.kelling@hzdr.de>
2019-10-03 16:51:38 +0200

    eurohack_env: Do module switch cuda after PYExtensions

--- eurohack_env.sh
+++ eurohack_env.sh
@@ -3,7 +3,7 @@
 module load daint-gpu
 module switch PrgEnv-cray PrgEnv-gnu
 module load craype-accel-nvidia60
-module switch cudatoolkit cudatoolkit/9.2.148_3.19-6.0.7.1_2.1__g3d9acc8
 module load PyExtensions/3.6.5.1-CrayGNU-18.08
+module switch cudatoolkit cudatoolkit/9.2.148_3.19-6.0.7.1_2.1__g3d9acc8
 
 export CPATH=$CRAY_MPICH_DIR/include
