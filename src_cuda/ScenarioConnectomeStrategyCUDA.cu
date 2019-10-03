131a2f210ee127fba4b8b2f1bbc745cf99979b53
Jeffrey Kelling <j.kelling@hzdr.de>
2019-10-03 13:43:17 +0200

    ScenarioConnectomeStrategyCUDA::derivetie: Fix alignment
    with pitched mem

--- src_cuda/ScenarioConnectomeStrategyCUDA.cu
+++ src_cuda/ScenarioConnectomeStrategyCUDA.cu
@@ -192,23 +192,16 @@ static __device__ Coord find_coord(int i)
 static __global__ void derivativeKernel(const double* agent, double* deriv, const int* type, const ScenarioConnectomeStrategyCUDA::array_properties arr_prop, int nall,
 double dt, int step)
 {
-  const int ii = threadIdx.x + blockDim.x*blockIdx.x +1;
+  const int ii = threadIdx.x + blockDim.x*blockIdx.x;
   const int jj = blockIdx.y +1;
   const int kk = blockIdx.z +1;
   nall = pitch.pDouble * (nvl[1]+2)*(nvl[2]+2);
-  if(ii < nvl[0]+1)
+  if(ii > 0 && ii < nvl[0]+1)
   {
     const int i = find_id(ii,jj,kk);
 
     if (type[i] & tissue(EMP)) return;
 
-  //const int ii = threadIdx.x + blockDim.x*blockIdx.x +1;
-  //const int jj = blockIdx.y +1;
-  //const int kk = blockIdx.z +1;
-	//if(ii < nvl[0]+1)
-	//{
-  //const int i = find_id(ii,jj,kk);
-  //if (type[i] & tissue(EMP)) return;
   // direct function or time derivatives
 
 	const auto ag_sAb = agent[sAb * nall + i];
