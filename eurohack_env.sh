f5c91dc1472cff05124266c9ad01c245770d8c4c
Hck Course <hck07@daint104.login.cscs.ch>
2019-10-03 16:11:15 +0200

    OpenACC environment

--- eurohack_env.sh
+++ eurohack_env.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 module load daint-gpu
-module switch PrgEnv-cray PrgEnv-gnu
+module load cdt/19.08
+module switch PrgEnv-cray PrgEnv-pgi
+#module switch PrgEnv-cray PrgEnv-gnu
 module load craype-accel-nvidia60
-module load PyExtensions/3.6.5.1-CrayGNU-18.08
-
+#module load PyExtensions/3.6.5.1-CrayGNU-18.08
+export LD_LIBRARY_PATH=$CRAY_LD_LIBRARY_PATH:$LD_LIBRARY_PATH
